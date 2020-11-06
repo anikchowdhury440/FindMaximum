25b7e6f71ca97dfee16f78994f32b77c08ce34ef
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-06 17:22:29 +0530

    Added: Test Cases to find the maximum of three number

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,13 +1,24 @@
 
 public class FindMaximum {
 	
-	public Integer getMaximum(Integer number1, Integer number2, Integer number3) {
-		Integer max = number1;
-		if(number2.compareTo(max) > 0) {
-			max = number2;
+	public Integer getMaximum(Integer integer1, Integer integer2, Integer integer3) {
+		Integer max = integer1;
+		if(integer2.compareTo(max) > 0) {
+			max = integer2;
 		}
-		if(number3.compareTo(max) > 0) {
-			max = number3;
+		if(integer3.compareTo(max) > 0) {
+			max = integer3;
+		}
+		return max;
+	}
+
+	public Float getMaximum(Float float1, Float float2, Float float3) {
+		Float max = float1;
+		if(float2.compareTo(max) > 0) {
+			max = float2;
+		}
+		if(float3.compareTo(max) > 0) {
+			max = float3;
 		}
 		return max;
 	}
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -5,25 +5,49 @@ public class FindMaximumTest {
 	
 	@Test
 	public void testMaxInteger_PresentAtFirstPosition_ShouldReturnSameInteger() {
-		int integer1 = 57, integer2 = 23, integer3 = 32;
+		Integer integer1 = 57, integer2 = 23, integer3 = 32;
 		FindMaximum findMaximum = new FindMaximum();
-		int max = findMaximum.getMaximum(integer1, integer2, integer3);
+		Integer max = findMaximum.getMaximum(integer1, integer2, integer3);
 		Assert.assertEquals(integer1, max);
 	}
 
 	@Test
 	public void testMaxInteger_PresentAtSecondPosition_ShouldReturnSameInteger() {
-		int integer1 = 57, integer2 = 83, integer3 = 32;
+		Integer integer1 = 57, integer2 = 83, integer3 = 32;
 		FindMaximum findMaximum = new FindMaximum();
-		int max = findMaximum.getMaximum(integer1, integer2, integer3);
+		Integer max = findMaximum.getMaximum(integer1, integer2, integer3);
 		Assert.assertEquals(integer2, max);
 	}
 	
 	@Test
 	public void testMaxInteger_PresentAtThirdPosition_ShouldReturnSameInteger() {
-		int integer1 = 57, integer2 = 23, integer3 = 82;
+		Integer integer1 = 57, integer2 = 23, integer3 = 82;
 		FindMaximum findMaximum = new FindMaximum();
-		int max = findMaximum.getMaximum(integer1, integer2, integer3);
+		Integer max = findMaximum.getMaximum(integer1, integer2, integer3);
 		Assert.assertEquals(integer3, max);
+	}
+	
+	@Test
+	public void testMaxFloat_PresentAtFirstPosition_ShouldReturnSameFloat() {
+		Float float1 = 57.41f, float2 = 23.93f, float3 = 32.24f;
+		FindMaximum findMaximum = new FindMaximum();
+		Float max = findMaximum.getMaximum(float1, float2, float3);
+		Assert.assertEquals(float1, max);
+	}
+
+	@Test
+	public void testMaxFloat_PresentAtSecondPosition_ShouldReturnSameFloat() {
+		Float float1 = 57.41f, float2 = 83.93f, float3 = 32.24f;
+		FindMaximum findMaximum = new FindMaximum();
+		Float max = findMaximum.getMaximum(float1, float2, float3);
+		Assert.assertEquals(float2, max);
+	}
+	
+	@Test
+	public void testMaxFloat_PresentAtThirdPosition_ShouldReturnSameFloat() {
+		Float float1 = 57.41f, float2 = 23.93f, float3 = 82.24f;
+		FindMaximum findMaximum = new FindMaximum();
+		Float max = findMaximum.getMaximum(float1, float2, float3);
+		Assert.assertEquals(float3, max);
 	}
 }
