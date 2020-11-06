6e9668ffff8d750ac0d6069fd0d0bf870b121bd4
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-06 17:54:58 +0530

    Refactored: All the 3 methods into 1 generic method

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,35 +1,13 @@
 
 public class FindMaximum {
 	
-	public Integer getMaximum(Integer integer1, Integer integer2, Integer integer3) {
-		Integer max = integer1;
-		if(integer2.compareTo(max) > 0) {
-			max = integer2;
+	public <E extends Comparable<E>> E getMaximum(E value1, E value2, E value3) {
+		E max = value1;
+		if(value2.compareTo(max) > 0) {
+			max = value2;
 		}
-		if(integer3.compareTo(max) > 0) {
-			max = integer3;
-		}
-		return max;
-	}
-
-	public Float getMaximum(Float float1, Float float2, Float float3) {
-		Float max = float1;
-		if(float2.compareTo(max) > 0) {
-			max = float2;
-		}
-		if(float3.compareTo(max) > 0) {
-			max = float3;
-		}
-		return max;
-	}
-
-	public String getMaximum(String string1, String string2, String string3) {
-		String max = string1;
-		if(string2.compareTo(max) > 0) {
-			max = string2;
-		}
-		if(string3.compareTo(max) > 0) {
-			max = string3;
+		if(value3.compareTo(max) > 0) {
+			max = value3;
 		}
 		return max;
 	}
