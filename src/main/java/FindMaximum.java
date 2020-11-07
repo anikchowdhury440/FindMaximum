fde17a122c7b72e0816926bf0b862d65f5a32a64
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-07 12:37:59 +0530

    Refactored: Using sort function

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -9,29 +9,15 @@ public class FindMaximum<E extends Comparable<E>> {
 	}
 	
 	public static <E extends Comparable<E>> E getMaximum(E[] values) {
-		
-		E max = values[0];
-	
-		for(E value : values) {
-			if(value.compareTo(max) > 0) {
-				max = value;
-			}
-		} 
-	
-		printMax(values, max);
-		return max;
-		
+		E[] sortedValues = values;
+		Arrays.sort(sortedValues);
+		int size = values.length;
+		E max = values[size-1];
+		return max;	
 	}
 	
 	public E getMaximum() {
 		return FindMaximum.getMaximum(values);
 	}
 	
-	public static <E> void printMax(E[] values, E max) {
-		System.out.print("Maximum of ");
-		for(E value: values) {
-			System.out.print(value + " ");
-		}
-		System.out.println("is " + max);
-	}
 }
